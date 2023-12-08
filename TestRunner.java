0b16725e0977c23a4c3a2aba950215bc82cf52e3
mojkko <77023631+mojkko@users.noreply.github.com>
2023-12-08 09:24:46 +0100

    Update TestRunner.java

--- TestRunner.java
+++ TestRunner.java
@@ -1,9 +1,15 @@
 package webAutomation;
 
+// TestRunner class for executing the GoogleSearch test
 public class TestRunner {
-
+    
+    // Main method to run the GoogleSearch test
     public static void main(String[] args) {
+        
+        // Create an instance of the GoogleSearch class
         GoogleSearch googleSearchTest = new GoogleSearch();
+        
+        // Call the googleSearch method to perform the test
         googleSearchTest.googleSearch();
     }
 }
