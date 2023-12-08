b377c73ba514972984b529a9cc1c18070fb79dfd
mojkko <77023631+mojkko@users.noreply.github.com>
2023-12-08 09:12:56 +0100

    Update Base.java

--- Base.java
+++ Base.java
@@ -3,9 +3,12 @@ package webAutomation;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+// Base class provides a WebDriver object for test classes to use
 public class Base {
+     // Returns a WebDriver object, which is assumed to be initialized elsewhere
     protected WebDriver getDriver() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\HegedusMojmir\\Selenium_Drivers\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        // WebDriver is created here
         return new ChromeDriver();
     }
 
