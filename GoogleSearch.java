c9dad1e85df1cf435a8fb0a9990906cc4faa148a
mojkko <77023631+mojkko@users.noreply.github.com>
2023-12-08 09:22:06 +0100

    Update GoogleSearch.java

--- GoogleSearch.java
+++ GoogleSearch.java
@@ -7,21 +7,30 @@ import org.openqa.selenium.WebElement;
 public class GoogleSearch extends Base {
 
     public void googleSearch() {
+        
         // Get the WebDriver object from the Base class
         WebDriver driver = getDriver();
-
+        
+        // Go to google home page
         driver.get("https://www.google.com");
-
+        
+        // Call the method to accept all cookies on the page 
         acceptAllCookies(driver);
 
+        // Find the search input field by its 'name' attribute
         WebElement searchBox = driver.findElement(By.name("q"));
+
+        // Enter search term "Active sync" into input field
         searchBox.sendKeys("Active sync");
+
+        // Submit the search form
         searchBox.submit();
 
+        // Find and click on the first search result link, assuming it's represented by an h3 element
         WebElement firstLink = driver.findElement(By.cssSelector("h3"));
         firstLink.click();
 
-
+    // Private method to handle accepting all coockies 
     }
     private void acceptAllCookies(WebDriver driver) {
         try {
